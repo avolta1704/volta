878a0d6a62a3b6aec134f847050301a72426928d
eleoromario <eleo.romario@gmail.com>
2024-05-16 10:45:29 -0500

    Actualizar método para obtener el pago de matrícula de un postulante

--- controller/admisionalumno.controller.php
+++ controller/admisionalumno.controller.php
@@ -220,7 +220,7 @@ class ControllerAdmisionAlumno
       for ($i = 3; $i <= 12; $i++) {
         $mesPago = $meses[$i];
         $ultimoDia = date("t", mktime(0, 0, 0, $i, 1, date("Y")));
-        $fechaLimite = date("Y"). '-' . $i . '-' . $ultimoDia;
+        $fechaLimite = date("Y") . '-' . $i . '-' . $ultimoDia;
 
         $dataCronoPagoPension = array(
           "idAdmisionAlumno" => $codAdmisionAlumno,
@@ -299,5 +299,120 @@ class ControllerAdmisionAlumno
     $tabla = "cronograma_pago";
     $dataCalendarioPagos = ModelAdmisionAlumno::mdlGetCalendarioPagos($tabla, $codAdmisionAlumno);
     return $dataCalendarioPagos;
+  }
+
+  public static function ctrActualizarestadoAdmisionAlumnoCancelado($codAdmisionAlumno)
+  {
+
+    // Obtener el registro de anio_escolar
+    $dataAnioEscolar = self::ctrOGetDataAnioEscolar();
+    if ($dataAnioEscolar) {
+      //sesión esté iniciada
+      if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+      }
+      // acceder a la variable de sesión
+      $idUsuario = $_SESSION["idUsuario"];
+
+      $tabla = "cronograma_pago";
+      //  crear solo una vez este array de datos por que es la matriculado
+      $dataCronoPagoMatricula = array(
+        "idAdmisionAlumno" => $codAdmisionAlumno,
+        "conceptoPago" => "Matrícula",
+        "montoPago" => $dataAnioEscolar["costoMatricula"],
+        "fechaLimite" => "2021-03-05",
+        "estadoCronograma" => 2,
+        "mesPago" => "Matricula",
+        "fechaCreacion" => date("Y-m-d H:i:s"),
+        "fechaActualizacion" => date("Y-m-d H:i:s"),
+        "usuarioCreacion" => $idUsuario,
+        "usuarioActualizacion" => $idUsuario
+      );
+      //crear 11 veces este array de datos por que es la Pension pero se tiene que crear desde marzo hasta diciembre
+      $dataAllCronoPago = array();
+      $dataAllCronoPago[] = $dataCronoPagoMatricula;
+      $meses = array(
+        1 => "Enero",
+        2 => "Febrero",
+        3 => "Marzo",
+        4 => "Abril",
+        5 => "Mayo",
+        6 => "Junio",
+        7 => "Julio",
+        8 => "Agosto",
+        9 => "Septiembre",
+        10 => "Octubre",
+        11 => "Noviembre",
+        12 => "Diciembre"
+      );
+
+      for ($i = 3; $i <= 12; $i++) {
+        $mesPago = $meses[$i];
+        $ultimoDia = date("t", mktime(0, 0, 0, $i, 1, date("Y")));
+        $fechaLimite = date("Y") . '-' . $i . '-' . $ultimoDia;
+
+        $dataCronoPagoPension = array(
+          "idAdmisionAlumno" => $codAdmisionAlumno,
+          "conceptoPago" => "Pensión",
+          "montoPago" => $dataAnioEscolar["costoPension"],
+          "fechaLimite" => $fechaLimite,
+          "estadoCronograma" => 1,
+          "mesPago" => $mesPago,
+          "fechaCreacion" => date("Y-m-d H:i:s"),
+          "fechaActualizacion" => date("Y-m-d H:i:s"),
+          "usuarioCreacion" => $idUsuario,
+          "usuarioActualizacion" => $idUsuario
+        );
+        $dataAllCronoPago[] = $dataCronoPagoPension;
+      }
+      foreach ($dataAllCronoPago as $dataAdmisionCronoPago) {
+        $response = ModelAdmisionAlumno::mdlCrearCronogramaPago($tabla, $dataAdmisionCronoPago);
+        if ($response != "ok") {
+          return "error";
+        }
+      }
+      // Actualizar el campo en la tabla admision_alumno
+      $table = "admision_alumno";
+      $dataActualizarEstadoAdAlum = array(
+        "idAdmisionAlumno" => $codAdmisionAlumno,
+        "estadoAdmisionAlumno" => 2, //estado por defecto 1 = registrado 2 = establecido 3 = cancelado
+        "fechaActualizacion" => date("Y-m-d H:i:s"),
+        "usuarioActualizacion" => $idUsuario
+      );
+      $response = ModelAdmisionAlumno::mdlActualizarestadoAdmisionAlumno($table, $dataActualizarEstadoAdAlum);
+      if ($response == "ok") {
+        return "ok";
+      } else {
+        return "error";
+      }
+    } else {
+      return "error";
+    }
+  }
+
+  /**
+   * Obtiene el código de admisión del alumno por admisión.
+   *
+   * @param string $codAdmisioon El código de admisión.
+   * @return mixed El código de admisión del alumno o null si no se encuentra.
+   */
+  public static function ctrGetCodAdmisionAlumnoByAdmision($codAdmisioon)
+  {
+    $tabla = "admision_alumno";
+    $result = ModelAdmisionAlumno::mdlGetCodAdmisionAlumnoByAdmision($tabla, $codAdmisioon);
+    return $result;
+  }
+
+  /**
+   * Obtiene el código del cronograma de matrícula por código de admisión de alumno.
+   *
+   * @param int $codAdmisionAlumno El código de admisión del alumno.
+   * @return mixed El código del cronograma de matrícula o null si no se encuentra.
+   */
+  public static function ctrGetCodeCronogramaMatriculaByCodAdmisionAlumno($codAdmisionAlumno)
+  {
+    $tabla = "cronograma_pago";
+    $result = ModelAdmisionAlumno::mdlGetCodeCronogramaMatriculaByCodAdmisionAlumno($tabla, $codAdmisionAlumno);
+    return $result;
   }
 }
