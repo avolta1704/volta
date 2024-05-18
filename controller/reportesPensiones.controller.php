aa1fe2499bbb5b86b599b32668254cd7b45c058e
eleoromario <eleo.romario@gmail.com>
2024-05-18 10:21:13 -0500

    feat: Agregar método para obtener cronogramas de pago generales con el estado del alumno

--- controller/reportesPensiones.controller.php
+++ controller/reportesPensiones.controller.php
@@ -45,10 +45,46 @@ class ControllerReportesPensiones
     return $dataFinal;
   }
 
+  /**
+   * Obtiene los cronogramas de pago por rango de fechas.
+   *
+   * @return array Retorna un array con los cronogramas de pagos por rango de fechas.
+   */
+
+  public static function ctrGetCronogramasPagosPorRango()
+  {
+    // Obtener todos los cronogramas de pago de deuda pasada por alumno
+    $respuesta = ModelReportesPensiones::mdlGetCronogramasPagoPorRango();
+
+    $alumnosAgrupados = self::ctrAgruparPorAlumno($respuesta);
+    // Agrupar por idAlumno
+
+    $dataFinal = array();
+    foreach ($alumnosAgrupados as $alumno) {
+      // obtener todos los cronogramas de todos los alumnos con deuda pasada
+      $todosLosPensionesPendientes = ControllerPagos::ctrGetCronogramasPorIdAlumno($alumno["idAlumno"]);
+      // Agrupar por mes de pago
+      $mesesAlumno = self::ctrAgrupoMesesAlumno($todosLosPensionesPendientes);
+
+      $mesesAlumno["Alumno"] = $alumno["nombresAlumno"] . " " . $alumno["apellidosAlumno"];
+      $mesesAlumno["DNI"] = $alumno["dniAlumno"];
+      $mesesAlumno["Grado"] = $alumno["descripcionGrado"];
+      $mesesAlumno["Nivel"] = $alumno["descripcionNivel"];
+      $mesesAlumno["Estado"] = $alumno["estadoAlumno"];
+      $dataFinal[] = $mesesAlumno;
+    }
+
+    return $dataFinal;
+  }
+
+  /**
+   * Obtiene los cronogramas de pago pendientes por alumno.
+   *
+   * @return array Retorna un array con los cronogramas de pago pendientes por alumno.
+   */
   public static function ctrGetCronogramasPagoPendientesPorAlumno()
   {
     // Obtener todos los cronogramas de pago de deuda pasada por alumno
-    // $respuesta = ModelReportesPensiones::mdlGetCronogramasPagoPendientesPorAlumno();
     $respuesta = ModelReportesPensiones::mdlGetCronogramasPagoPendientes();
 
     $alumnosAgrupados = self::ctrAgruparPorAlumno($respuesta);
