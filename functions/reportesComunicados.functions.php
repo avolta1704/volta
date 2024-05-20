1d2cb0a465e1a70dacedc255473300890796c91b
eleoromario <eleo.romario@gmail.com>
2024-05-20 11:03:56 -0500

    feat: Añadir la funcion para eliminar datos duplicados

--- functions/reportesComunicados.functions.php
+++ functions/reportesComunicados.functions.php
@@ -2,7 +2,7 @@
 class FunctionReportesComunicados
 {
   //  Botones Opciones para la fila
-  public static function getBotonesOpciones($codComunicado, $codAlumno)
+  public static function getBotonesOpciones($codAdmisionAlumno, $codAlumno)
   {
     $botones = '
     <div class="btn-group">
@@ -10,10 +10,29 @@ class FunctionReportesComunicados
         <i class="bi bi-pencil-square"></i>
       </button>
       <ul class="dropdown-menu" aria-labelledby="dropDownAdmiAlumno">
-        <li><button type="button" class="dropdown-item btnVerComunicadosAlumno" codAlumnoComunicado="' . ($codComunicado) . '" codAlumno="' . ($codAlumno) . '"  data-bs-toggle="modal" data-bs-target="#btnVerComunicadosAlumno">Ver Comunicados</button></li>
+        <li><button type="button" class="dropdown-item btnVerComunicadosAlumno" codAlumnoComunicado="' . ($codAdmisionAlumno) . '" codAlumno="' . ($codAlumno) . '"  data-bs-toggle="modal" data-bs-target="#btnVerComunicadosAlumno">Ver Comunicados</button></li>
       </ul>
     </div>
     ';
     return $botones;
+  }
+
+  // Eliminar datos duplicados basados en idalumno
+  public static function eliminarDuplicadosPorIdAlumno($array)
+  {
+    $result = array();
+    $idsAlumno = array();
+
+    foreach ($array as $item) {
+      // Asegúrate de reemplazar 'idAlumno' con la clave real que se utiliza en tu array
+      $idAlumno = $item['idAlumno'];
+
+      if (!in_array($idAlumno, $idsAlumno)) {
+        $idsAlumno[] = $idAlumno;
+        $result[] = $item;
+      }
+    }
+
+    return $result;
   }
 }
