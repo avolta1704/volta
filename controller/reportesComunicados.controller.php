c581fec6643352d086791cd23a365a0a5c05d0a4
eleoromario <eleo.romario@gmail.com>
2024-05-20 14:57:38 -0500

    feat: Agregar función para agrupar datos por alumno en reporte de comunicados

--- controller/reportesComunicados.controller.php
+++ controller/reportesComunicados.controller.php
@@ -15,4 +15,60 @@ class ControllerReportesComunicados
     $comunicados = ModelReportesComunicados::mdlGetAllAlumnosConComunicados($table);
     return $comunicados;
   }
+
+  /**
+   * Obtiene los comunicados por alumnos.
+   *
+   * @return array Retorna un array con los datos de los comunicados por alumnos.
+   */
+  public static function ctrGetComunicadosPorAlumnos()
+  {
+    $table = "comunicacion_pago";
+    $comunicadosPorAlumno = ModelReportesComunicados::mdlGetComunicadosPorAlumno($table);
+    if (empty($comunicadosPorAlumno)) {
+      return $comunicadosPorAlumno;
+    }
+    $comunicadosPorAlumno = self::ctrAgruparPorAlumno($comunicadosPorAlumno);
+    return $comunicadosPorAlumno;
+  }
+
+  /**
+   * Agrupa los datos por alumno.
+   *
+   * @param array $data Los datos a agrupar.
+   * @return void
+   */
+  public static function ctrAgruparPorAlumno($data)
+  {
+    $result = [];
+
+    foreach ($data as $alumno) {
+      $pension = ModelReportesComunicados::mdlGetPensionAlumno($alumno['idAdmisionAlumno']);
+      $id = $alumno['idAlumno'];
+      if (!isset($result[$id])) {
+        $result[$id] = [
+          'idAlumno' => $alumno['idAlumno'],
+          'Nombres' => $alumno['apellidosAlumno'] . ", " . $alumno['nombresAlumno'],
+          "Nivel" => $alumno['descripcionNivel'] . " " . $alumno['descripcionGrado'],
+          "Pension" => $pension['montoPago'],
+          'Mes Deuda' => [],
+          'Monto Deuda' => 0,
+        ];
+      }
+      // Añadir solo las tres primeras letras del mes
+      $result[$id]['Mes Deuda'][] = $alumno['mesPago'] == "Matricula" ? $alumno['mesPago'] : substr($alumno['mesPago'], 0, 3);
+      $result[$id]['Monto Deuda'] += $alumno['montoPago'];
+
+      // Añadir comunicado enumerado
+      $comunicadoIndex = count(array_filter(array_keys($result[$id]), fn ($key) => strpos($key, 'Comunicado') === 0)) + 1;
+      $result[$id]["Comunicado $comunicadoIndex"] = $alumno['fechaComunicacion'] . ': ' . $alumno['tituloComunicacion'] . ', ' . $alumno['detalleComunicacion'];
+    }
+
+    // Convertir mesesPago a una cadena de texto separada por comas
+    foreach ($result as &$alumno) {
+      $alumno['Mes Deuda'] = implode(', ', $alumno['Mes Deuda']);
+    }
+
+    return $result;
+  }
 }
