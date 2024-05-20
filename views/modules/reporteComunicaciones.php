861706ab7924e16afa02f6609f6c3be9b1b91e81
eleoromario <eleo.romario@gmail.com>
2024-05-20 10:36:13 -0500

    feat: Agregar vista de reporte de comunicaciones

---
+++ views/modules/reporteComunicaciones.php
@@ -0,0 +1,50 @@
+<main id="main" class="main">
+  <div class="pagetitle">
+    <h2 class="mt-4 tituloReportesComunicaciones"></h2><br>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
+        <li class="breadcrumb-item active">Reportes de Comunicaciones</li>
+      </ol>
+    </nav>
+  </div>
+
+  <section class="section dashboard">
+    <div class="row">
+      <div class="col-lg-2">
+        <div class="row mb-2">
+          <div class="dropdown">
+            <button class="btn btn-outline-primary dropdown-toggle d-flex gap-3 align-items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="bi bi-download"></i> Descargar Reporte
+            </button>
+            <ul class="dropdown-menu">
+              <li><button class="dropdown-item" id="btnDescargarReportePagos"><i class="bi bi-file-earmark-excel"></i> Descargar reporte de pagos</button></li>
+              <li><button class="dropdown-item" id="btnDescargarReporteInicial"><i class="bi bi-file-earmark-excel"></i> Descargar retrasos de inicial</button></li>
+              <li><button class="dropdown-item" id="btnDescargarReportePrimaria"><i class="bi bi-file-earmark-excel"></i> Descargar retrasos de primaria</button></li>
+              <li><button class="dropdown-item" id="btnDescargarReporteSecundaria"><i class="bi bi-file-earmark-excel"></i> Descargar retrasos de secundaria</button></li>
+              <li><button class="dropdown-item" data-bs-toggle="modal" data-bs-target="#seleccionarRangoFechas"><i class="bi bi-file-earmark-excel"></i> Descargar deudores por rango de meses</button></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- Left side columns -->
+      <div class="col-lg-12">
+        <div class="row">
+          <div class="card">
+            <div class="card-body">
+              <!--  Titulo dataTableReportesComunicacionesAdmin-->
+              <table id="dataTableReportesComunicaciones" class="display dataTableReportesComunicaciones" style="width: 100%">
+                <thead>
+                  <!-- dataTableReportesComunicacionesAdmin -->
+                </thead>
+                <tbody>
+                  <!--dataTableReportesComunicacionesAdmin-->
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
