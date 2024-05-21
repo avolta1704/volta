3172e3769e007d55838f66fff266b3e87e85c5ce
Alx <worfysito15@gmail.com>
2024-05-21 13:02:32 -0500

    lista postulantes anio

---
+++ views/modules/listaPostulantes copy.php
@@ -0,0 +1,71 @@
+<main id="main" class="main">
+
+  <div class="pagetitle">
+    <h2 class="mt-4 tituloPostulantes"></h2><br>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
+        <li class="breadcrumb-item active">Todos los Postulantes</li>
+      </ol>
+    </nav>
+  </div>
+
+  <section class="section dashboard">
+    <div class="row">
+      <div class="col-lg-2">
+        <div class="row mb-2">
+          <button type="button" class="btn btn-primary btnAgregarPostulante" id="btnAgregarPostulante">Nuevo
+            Postulante</button>
+        </div>
+      </div>
+      <!-- Left side columns -->
+      <div class="col-lg-12">
+        <div class="row">
+          <div class="card">
+            <div class="card-body">
+              <!--  Titulo dataTablePostulantesAdmin-->
+              <table id="dataTablePostulantes" class="display dataTablePostulantes" style="width: 100%">
+                <thead>
+                  <!-- dataTablePostulantesAdmin -->
+                </thead>
+                <tbody>
+                  <!--dataTablePostulantesAdmin-->
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php
+$postulante = new ControllerPostulantes();
+$postulante->ctrBorrarPostulante();
+?>
+
+<div class="modal fade" id="actualizarEstado" aria-hidden="true" aria-labelledby="actualizarEstado" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered modal-sm">
+    <div class="modal-content tablaActualizaEstadoPostulante">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Actualizar Postulante</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <input type="hidden" id="codPostulante">
+        <select class="form-control" name="estadoPostulante" id="estadoPostulante">
+          <!-- <option value=""></option> -->
+          <option value="1">Registrado</option>
+          <option value="2">En Revisión</option>
+          <option value="3">Aprobado</option>
+          <option value="4">Rechazado</option>
+        </select>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary btnActualizarEstado" id="btnActualizarEstadoPostulante">Actualizar</button>
+      </div>
+    </div>
+  </div>
+</div>
