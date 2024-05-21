652197db8c84cbe15a12c17f5fc52e79082f36af
eleoromario <eleo.romario@gmail.com>
2024-05-21 16:31:00 -0500

    feat: Se añade la funcion registrar un nuevo curso al controlador

--- controller/cursos.controller.php
+++ controller/cursos.controller.php
@@ -11,4 +11,30 @@ class ControllerCursos
     $response = ModelCursos::mdlGetCursos();
     return $response;
   }
+
+  /**
+   * Registra un curso.
+   *
+   * @param array $data Datos del curso.
+   * @return string Retorna un mensaje de éxito o error.
+   */
+  public static function ctrRegistrarCurso($data)
+  {
+    if (session_status() == PHP_SESSION_NONE) {
+      session_start();
+    }
+
+    $idUsuario = $_SESSION['idUsuario'];
+    $dataRegistrarCursoModal = array(
+      "descripcionCurso" => $data["descripcionCurso"],
+      "idArea" => $data["idArea"],
+      "usuarioCreacion" => $idUsuario,
+      "fechaCreacion" => date('Y-m-d H:i:s'),
+      "usuarioActualizacion" => $idUsuario,
+      "fechaActualizacion" => date('Y-m-d H:i:s'),
+    );
+
+    $response = ModelCursos::mdlRegistrarCurso($dataRegistrarCursoModal);
+    return $response;
+  }
 }
