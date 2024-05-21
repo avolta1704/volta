5613120146be3edf19158754980bbe8683d1ca0d
eleoromario <eleo.romario@gmail.com>
2024-05-21 16:36:11 -0500

    fix: Quitar de la consulta SQL para obtener cursos con áreas sin grado

--- model/cursos.model.php
+++ model/cursos.model.php
@@ -12,13 +12,9 @@ class ModelCursos
   {
     $tabla = "curso";
     $tablaArea = "area";
-    $tablaCursoGrado = "curso_grado";
-    $tablaGrado = "grado";
-    $stmt = Connection::conn()->prepare("SELECT c.descripcionCurso, c.idCurso, a.descripcionArea, g.descripcionGrado, c.estadoCurso
+    $stmt = Connection::conn()->prepare("SELECT c.descripcionCurso, c.idCurso, a.descripcionArea, c.estadoCurso
                 FROM $tabla c 
                 INNER JOIN $tablaArea a ON c.idArea = a.idArea
-                INNER JOIN $tablaCursoGrado cg ON c.idCurso = cg.idCurso
-                INNER JOIN $tablaGrado g ON cg.idGrado = g.idGrado
                 ORDER BY c.idCurso DESC
                 ");
     $stmt->execute();
