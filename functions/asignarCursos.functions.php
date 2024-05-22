8905dc16ee8c5d9098afb82a2ca5362afe64effe
eleoromario <eleo.romario@gmail.com>
2024-05-22 10:10:08 -0500

    feat: Agregar funcion para crear el boton de ver cursos

---
+++ functions/asignarCursos.functions.php
@@ -0,0 +1,10 @@
+<?php
+class FunctionAsignarCursos
+{
+  public static function getButtonListarCursos()
+  {
+    return  '<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalListarCursos">
+            Ver Cursos
+          </button>';
+  }
+}
