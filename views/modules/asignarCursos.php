9fe63961a4494fcf38f3f11b8634a3a8086063b4
eleoromario <eleo.romario@gmail.com>
2024-05-22 10:11:41 -0500

    feat: Agregar vista y funcionalidad para asignar cursos

---
+++ views/modules/asignarCursos.php
@@ -0,0 +1,35 @@
+<main id="main" class="main">
+
+  <div class="pagetitle">
+    <h2 class="mt-4 tituloAsignarCursos"></h2><br>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
+        <li class="breadcrumb-item active">Asignar Cursos</li>
+      </ol>
+    </nav>
+  </div>
+
+  <section class="section dashboard">
+    <div class="row gap-3">
+      <!-- Left side columns -->
+      <div class="col-lg-12">
+        <div class="row">
+          <div class="card">
+            <div class="card-body">
+              <!--  Titulo dataTableAsignarCursosAdmin-->
+              <table id="dataTableAsignarCursos" class="display dataTableAsignarCursos " style="width: 100%">
+                <thead>
+                  <!-- dataTableAsignarCursosAdmin -->
+                </thead>
+                <tbody>
+                  <!--dataTableAsignarCursosAdmin-->
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
