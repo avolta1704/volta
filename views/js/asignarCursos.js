edd677eefc15013e77f41c27ccfd9ae57844bd36
eleoromario <eleo.romario@gmail.com>
2024-05-22 14:48:37 -0500

    feat: Agregar funcionalidad para eliminar asignación de curso por grado en ajax

--- views/js/asignarCursos.js
+++ views/js/asignarCursos.js
@@ -106,3 +106,79 @@ $("#btnGuardarAsignacionCurso").on("click", function () {
 		},
 	});
 });
+
+// Eliminar asignacion de curso
+$("#dataTableCursosPorGrado").on(
+	"click",
+	"#btnEliminarCursoAsignado",
+	function () {
+		// Obtener idCursoGrado del btnEliminarCursoAsignado
+		const idCursoGrado = $(this).attr("idCursoGrado");
+
+		const dataEliminarCurso = {
+			idCursoGrado: idCursoGrado,
+			btnEliminarCursoAsignado: true,
+		};
+
+		Swal.fire({
+			title: "¿Estás seguro de eliminar la asignación de este curso?",
+			text: "¡Esta acción no se puede revertir!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Sí, eliminar",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				//ajax para eliminar asignacion de curso
+				var data = new FormData();
+				data.append(
+					"dataEliminarCurso",
+					JSON.stringify(dataEliminarCurso)
+				);
+
+				$.ajax({
+					url: "ajax/asignarCursos.ajax.php",
+					method: "POST",
+					data: data,
+					cache: false,
+					contentType: false,
+					processData: false,
+					dataType: "json",
+					success: function (response) {
+						if (response == "ok") {
+							// Cerrar modal
+							$("#modalListarCursos").modal("hide");
+
+							// Mostrar mensaje de exito
+							Swal.fire({
+								icon: "success",
+								title: "¡Eliminación de curso exitosa!",
+								showConfirmButton: false,
+								timer: 1500,
+							}).then((result) => {
+								location.reload();
+							});
+						} else {
+							// Mostrar mensaje de error
+							Swal.fire({
+								icon: "error",
+								title: "¡Error al eliminar curso!",
+								showConfirmButton: false,
+								timer: 1500,
+							});
+						}
+					},
+					error: function (jqXHR, textStatus, errorThrown) {
+						console.log(jqXHR.responseText); // procendecia de error
+						console.log(
+							"Error en la solicitud AJAX: ",
+							textStatus,
+							errorThrown
+						);
+					},
+				});
+			}
+		});
+	}
+);
