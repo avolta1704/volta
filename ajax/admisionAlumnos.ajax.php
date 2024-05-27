fdd85e658e404796663ad257aae0e41d6850989d
eleoromario <eleo.romario@gmail.com>
2024-05-27 17:27:21 -0500

    feat: Actualizar estado de admisión de alumno

--- ajax/admisionAlumnos.ajax.php
+++ ajax/admisionAlumnos.ajax.php
@@ -46,6 +46,20 @@ class AdmisionAlumnosAjax
     $response = ControllerAdmision::ctrElimarDataMatriculaPostulante($codAlumnoEliminar);
     echo json_encode($response);
   }
+
+  /**
+   * Actualizar estado de admision alumno
+   * 
+   * @param data $_POST["editarEstadoAdmisionAlumno"]
+   * @return string ok si es correcto o error si no se actualiza
+   */
+  public function ajaxEditarEstadoAdmisionAlumno($data)
+  {
+
+    $data = json_decode($data, true);
+    $response = ControllerAdmisionAlumno::ctrEditarEstadoAdmisionAlumno($data);
+    echo json_encode($response);
+  }
 }
 
 // Mostar todos los registros de admision  dataTableAdmisionAlumnos
@@ -71,4 +85,10 @@ if (isset($_POST["codAlumnoEliminar"])) {
   $obtenerAlumnoDataEliminar = new AdmisionAlumnosAjax();
   $obtenerAlumnoDataEliminar->codAlumnoEliminar = $_POST["codAlumnoEliminar"];
   $obtenerAlumnoDataEliminar->ajaxElimnarAlumno();
+}
+
+// Editar estado de admision alumno
+if (isset($_POST["editarEstadoAdmisionAlumno"])) {
+  $codAdmisionAlumnoEditar = new AdmisionAlumnosAjax();
+  $codAdmisionAlumnoEditar->ajaxEditarEstadoAdmisionAlumno($_POST["editarEstadoAdmisionAlumno"]);
 }
