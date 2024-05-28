cda91931c3f45edd2facb25ea20fd1da9f0456f2
unknown <72468973@ucsm.edu.pe>
2024-05-28 16:52:49 -0500

    Adaptar los botones de Cursos con un dropdown para la vista cursos

--- functions/cursos.functions.php
+++ functions/cursos.functions.php
@@ -9,7 +9,21 @@ class FunctionCursos
    */
   public static function getBotonesCursos($idCurso)
   {
-    $botones = "<div class='btn-group'><button class='btn btn-warning btnEditarCurso' idCurso='$idCurso' data-bs-toggle='modal' data-bs-target='#modalEditarCurso'><i class='bi bi-pencil-square'></i></button><button class='btn btn-danger btnEliminarCurso' idCurso='$idCurso'><i class='bi bi-trash'></i></button></div>";
+/*     $botones = "<div class='btn-group'><button class='btn btn-warning btnEditarCurso' idCurso='$idCurso' data-bs-toggle='modal' data-bs-target='#modalEditarCurso'><i class='bi bi-pencil-square'></i></button><button class='btn btn-danger btnEliminarCurso' idCurso='$idCurso'><i class='bi bi-trash'></i></button></div>"; */
+
+    $botones = '
+      <div class="btn-group">
+        <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" id="dropDownPagos" aria-expanded="false">
+          <i class="bi bi-pencil-square"></i>
+        </button>
+      <ul class="dropdown-menu" aria-labelledby="dropDownPostulantes">
+        <div>
+          <button type="button" class="dropdown-item btnEditarCurso" idCurso="' . ($idCurso) . '" data-bs-toggle="modal" data-bs-target="#modalEditarCurso">Editar</button>
+        </div>
+        <div>
+            <button type="button" class="dropdown-item btnEliminarCurso" idCurso="' . ($idCurso) . '">Eliminar</button>
+        </div>
+        ';
     return $botones;
   }
 
