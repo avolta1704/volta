96ec76834e936cb51335e29b9492855edb858cde
eleoromario <eleo.romario@gmail.com>
2024-05-29 09:24:59 -0500

    feat: Agregar método en el modelo para obtener todos los grados por nivel

--- model/nivelGrado.model.php
+++ model/nivelGrado.model.php
@@ -6,7 +6,6 @@ class ModelNivelGrado
   //  Obtener todos los niveles
   public static function mdlGetAllNiveles($tabla)
   {
-    
   }
 
   //  Obtener todos los grados
@@ -22,6 +21,18 @@ class ModelNivelGrado
   {
     $statement = Connection::conn()->prepare("SELECT grado.idGrado, grado.descripcionGrado FROM $tabla WHERE idNivel = :idNivel");
     $statement->bindParam(":idNivel", $codNivel, PDO::PARAM_INT);
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  /**
+   * Obtener todos los grados por nivel
+   * 
+   * @return array/string $respuesta Retorna un array con los datos de los grados o un string con un mensaje de error.
+   */
+  public static function mdlGetAllGradosByNivel($tabla)
+  {
+    $statement = Connection::conn()->prepare("SELECT grado.idGrado, grado.descripcionGrado, nivel.descripcionNivel FROM $tabla AS grado INNER JOIN nivel ON grado.idNivel = nivel.idNivel");
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
   }
