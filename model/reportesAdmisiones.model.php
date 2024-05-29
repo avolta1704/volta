b0efa0b50073f3bcd5b48d14eaaddada8f72a061
eleoromario <eleo.romario@gmail.com>
2024-05-29 09:25:12 -0500

    feat: Agregar método para obtener reporte de admisiones por año lectivo

--- model/reportesAdmisiones.model.php
+++ model/reportesAdmisiones.model.php
@@ -26,4 +26,37 @@ class ModelReportesAdmisiones
       return "error";
     }
   }
+
+  /**
+   * Obtiene el reporte de admisiones por año lectivo.
+   * 
+   * @param string $tabla El nombre de la tabla en la base de datos.
+   * @param array $aniosLectivo Array de los años seleccionados.
+   * @return array/string El reporte de admisiones por año lectivo.
+   */
+  static public function mdlGetReportesPorAnioLectivo($tabla, $aniosLectivo)
+  {
+    $tablaAdmision = "admision";
+    $tablaAlumno = "alumno";
+    $tablaAnioAdmision = "anio_admision";
+    $tablaAnioEscolar = "anio_escolar";
+    $tablaAlumnoGrado = "alumno_grado";
+    $tablaGrado = "grado";
+    $tablaNivel = "nivel";
+    $stmt = Connection::conn()->prepare("SELECT aa.idAdmisionAlumno, a.idAnioEscolar, ae.estadoAnio, aa.idAlumno, aa.estadoAdmisionAlumno, al.nombresAlumno, al.apellidosAlumno, g.descripcionGrado, n.descripcionNivel
+      FROM $tabla AS aa
+      INNER JOIN $tablaAdmision AS a ON aa.idAdmision = a.idAdmision
+      INNER JOIN $tablaAnioAdmision AS ana ON a.idAnioEscolar = ana.idAnioEscolar
+      INNER JOIN $tablaAnioEscolar AS ae ON ana.idAnioEscolar = ae.idAnioEscolar
+      INNER JOIN $tablaAlumno AS al ON aa.idAlumno = al.idAlumno
+      INNER JOIN $tablaAlumnoGrado AS ag ON aa.idAlumno = ag.idAlumno
+      INNER JOIN $tablaGrado AS g ON ag.idGrado = g.idGrado
+      INNER JOIN $tablaNivel AS n ON g.idNivel = n.idNivel
+      WHERE ana.idAnioEscolar IN ($aniosLectivo)");
+    if ($stmt->execute()) {
+      return $stmt->fetchAll();
+    } else {
+      return "error";
+    }
+  }
 }
