d62fbe8348119881e41a9ef791fafa1b54c6e914
eleoromario <eleo.romario@gmail.com>
2024-05-29 09:23:47 -0500

    feat: Agregar método para obtener reporte de admisiones por año lectivo y mejorar estructura de la tabla de reportes

--- controller/reportesAdmisiones.controller.php
+++ controller/reportesAdmisiones.controller.php
@@ -12,4 +12,17 @@ class ControllerReportesAdmisiones
     $respuesta = ModelReportesAdmisiones::mdlGetReporteAdmisiones($tabla);
     return $respuesta;
   }
+
+  /**
+   * Obtiene el reporte de admisiones por año lectivo.
+   * 
+   * @param array $aniosLectivo Array de los años seleccionados.
+   * @return array/string $respuesta Retorna un array con los datos de las admisiones o un string con un mensaje de error.
+   */
+  static public function ctrGetReportesPorAnioLectivo($aniosLectivo)
+  {
+    $tabla = "admision_alumno";
+    $respuesta = ModelReportesAdmisiones::mdlGetReportesPorAnioLectivo($tabla, $aniosLectivo);
+    return $respuesta;
+  }
 }
