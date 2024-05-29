d0af6cd031817ee31778f97e9b9ca03c8bb37962
eleoromario <eleo.romario@gmail.com>
2024-05-29 13:07:27 -0500

    feat: Agregar tabla "anio_admision" y modificar consultas en el modelo de reportes de admisiones

--- model/reportesAdmisiones.model.php
+++ model/reportesAdmisiones.model.php
@@ -14,10 +14,12 @@ class ModelReportesAdmisiones
     $tablaAdmision = "admision";
     $tablaAlumno = "alumno";
     $tablaAñoEscolar = "anio_escolar";
+    $tablaAñoAdmision = "anio_admision";
     $stmt = Connection::conn()->prepare("SELECT aa.idAdmisionAlumno, a.idAnioEscolar, ae.estadoAnio, aa.idAlumno, aa.estadoAdmisionAlumno, al.nombresAlumno, al.apellidosAlumno
       FROM $tabla AS aa
       INNER JOIN $tablaAdmision AS a ON aa.idAdmision = a.idAdmision
-      INNER JOIN $tablaAñoEscolar AS ae ON a.idAnioEscolar = ae.idAnioEscolar
+      INNER JOIN $tablaAñoAdmision AS ana ON aa.idAdmisionAlumno = ana.idAdmisionAlumno
+      INNER JOIN $tablaAñoEscolar AS ae ON ana.idAnioEscolar = ae.idAnioEscolar
       INNER JOIN $tablaAlumno AS al ON aa.idAlumno = al.idAlumno
       WHERE ae.estadoAnio = 1");
     if ($stmt->execute()) {
@@ -53,6 +55,38 @@ class ModelReportesAdmisiones
       INNER JOIN $tablaGrado AS g ON ag.idGrado = g.idGrado
       INNER JOIN $tablaNivel AS n ON g.idNivel = n.idNivel
       WHERE ana.idAnioEscolar IN ($aniosLectivo)");
+    if ($stmt->execute()) {
+      return $stmt->fetchAll();
+    } else {
+      return "error";
+    }
+  }
+
+  /**
+   * Obtiene el reporte de nuevos y antiguos.
+   * 
+   * @param string $tabla El nombre de la tabla en la base de datos.
+   * @return array/string El reporte de nuevos y antiguos. 
+   */
+  static public function mdlGetReporteNuevosAntiguos($tabla)
+  {
+    $tablaAdmision = "admision";
+    $tablaAlumno = "alumno";
+    $tablaAnioAdmision = "anio_admision";
+    $tablaAnioEscolar = "anio_escolar";
+    $tablaAlumnoGrado = "alumno_grado";
+    $tablaGrado = "grado";
+    $tablaNivel = "nivel";
+    $stmt = Connection::conn()->prepare("SELECT aa.idAdmisionAlumno, a.idAnioEscolar, ae.estadoAnio, aa.idAlumno, aa.estadoAdmisionAlumno, al.nombresAlumno, al.apellidosAlumno, g.descripcionGrado, n.descripcionNivel,al.nuevoAlumno
+      FROM $tabla AS aa
+      INNER JOIN $tablaAdmision AS a ON aa.idAdmision = a.idAdmision
+      INNER JOIN $tablaAnioAdmision AS ana ON aa.idAdmisionAlumno = ana.idAdmisionAlumno
+      INNER JOIN $tablaAnioEscolar AS ae ON ana.idAnioEscolar = ae.idAnioEscolar
+      INNER JOIN $tablaAlumno AS al ON aa.idAlumno = al.idAlumno
+      INNER JOIN $tablaAlumnoGrado AS ag ON aa.idAlumno = ag.idAlumno
+      INNER JOIN $tablaGrado AS g ON ag.idGrado = g.idGrado
+      INNER JOIN $tablaNivel AS n ON g.idNivel = n.idNivel
+      WHERE ae.estadoAnio = 1 AND aa.estadoAdmisionAlumno = 2");
     if ($stmt->execute()) {
       return $stmt->fetchAll();
     } else {
