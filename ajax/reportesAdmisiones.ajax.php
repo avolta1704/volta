fd8d23f6cc9e2d0579fd7938f609afc505f22f79
eleoromario <eleo.romario@gmail.com>
2024-05-29 09:23:19 -0500

    feat: Agregar funcionalidad de reporte de admisiones por año lectivo

--- ajax/reportesAdmisiones.ajax.php
+++ ajax/reportesAdmisiones.ajax.php
@@ -3,6 +3,8 @@
 require_once "../controller/reportesAdmisiones.controller.php";
 require_once "../model/reportesAdmisiones.model.php";
 require_once "../functions/reportesAdmisiones.functions.php";
+require_once "../controller/nivelGrado.controller.php";
+require_once "../model/nivelGrado.model.php";
 
 class ReportesAdmisionesAjax
 {
@@ -16,9 +18,32 @@ class ReportesAdmisionesAjax
 
     echo json_encode($reporteAdmisiones);
   }
+
+  /**
+   * Mostrar reportes por año lectivo
+   * @param array $anioLectivo array de los años seleccionados
+   * @return result json 
+   */
+  public function ajaxMostrarReportesPorAnioLectivo($aniosLectivo)
+  {
+    $reportesPorAnioLectivo = ControllerReportesAdmisiones::ctrGetReportesPorAnioLectivo($aniosLectivo);
+    $grados = ControllerNivelGrado::ctrGetAllGradosByNivel();
+
+    $data = [
+      "reportesPorAnioLectivo" => $reportesPorAnioLectivo,
+      "grados" => $grados
+    ];
+
+    echo json_encode($data);
+  }
 }
 
 if (isset($_POST["todosLasAdmisiones"])) {
   $mostrarReporteAdmisiones = new ReportesAdmisionesAjax();
   $mostrarReporteAdmisiones->ajaxMostrarReporteAdmisiones();
+}
+
+if (isset($_POST["anioLectivo"])) {
+  $reportesPorAnioLectivo = new ReportesAdmisionesAjax();
+  $reportesPorAnioLectivo->ajaxMostrarReportesPorAnioLectivo($_POST["anioLectivo"]);
 }
