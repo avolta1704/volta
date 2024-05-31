c995d4a9a1bcca8c789ffef4f7967ba78498f6aa
eleoromario <eleo.romario@gmail.com>
2024-05-31 11:00:15 -0500

    feat: Agregar las funciones para obtener acciones de cursos y alumnos

---
+++ functions/cursosDocente.functions.php
@@ -0,0 +1,49 @@
+<?php
+
+class FunctionCursosDocente
+{
+
+  /**
+   * Obtiene las acciones de los cursos asignados al docente.
+   * 
+   */
+  static public function getAccionesCursosDocente($idCurso, $idGrado, $idPersonal)
+  {
+    $acciones = '
+    <div class="btn-group">
+      <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" id="dropDownAdmisiones" aria-expanded="false">
+        <i class="bi bi-pencil-square"></i>
+      </button>
+    <ul class="dropdown-menu" aria-labelledby="dropDownAdmisiones">
+    ';
+
+    $acciones .= '
+      <li><button type="button" class="dropdown-item btnVerAlumnosCurso" id="btnVerAlumnosCurso" data-bs-toggle="modal" data-bs-target="#modalListadoAlumnosCurso" idCurso="' . ($idCurso) . '" idGrado="' . ($idGrado) . '" idPersonal="' . ($idPersonal) . '" >Ver alumnos</button></li>
+      <li><button type="button" class="dropdown-item btn" idPerosnal="' . ($idPersonal) . '">Visualizar</button></li>
+      </ul>
+    </div>
+    ';
+    return $acciones;
+  }
+
+  /**
+   * Obtiene las acciones de los alumnos por curso
+   */
+  static public function getAccionesAlumnosPorCurso($idAlumno)
+  {
+    $acciones = '
+    <div class="btn-group">
+      <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" id="dropDownAdmisiones" aria-expanded="false">
+        <i class="bi bi-pencil-square"></i>
+      </button>
+    <ul class="dropdown-menu" aria-labelledby="dropDownAdmisiones">
+    ';
+
+    $acciones .= '
+      <li><button type="button" class="dropdown-item btn" idAlumno="' . ($idAlumno) . '">Visualizar</button></li>
+      </ul>
+    </div>
+    ';
+    return $acciones;
+  }
+}
