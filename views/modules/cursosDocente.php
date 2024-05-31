4f8a5c81bd236ea36513b53a9cfdac41debbe4f8
eleoromario <eleo.romario@gmail.com>
2024-05-31 13:26:24 -0500

    feat: Agregar modal para visualizar cronograma de pagos de un alumno en el curso del docente

--- views/modules/cursosDocente.php
+++ views/modules/cursosDocente.php
@@ -109,3 +109,22 @@
     </div>
   </div>
 </div>
+
+<!-- Modal visualizar cronograma del alumno -->
+<div class="modal fade" id="cronogramaAdmisionPago" data-bs-keyboard="false" tabindex="-1" aria-labelledby="cronogramaAdmisionPagoLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" style="max-width: 770px;">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="cronogramaAdmisionPagoLabel" style="padding-left: 150px; font-weight: bold;">
+          Cronograma Pagos Alumno</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- Genera 11 inputs para cada registro de Cronograma pago de estudiante -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
