d7c06035bfe911c681aa5e5b489f77d1a597a656
eleoromario <eleo.romario@gmail.com>
2024-06-05 16:04:03 -0500

    feat: Agregar botones para tomar asistencia, descargar template y subir excel asistencia

--- views/modules/visualizarAsistencia.php
+++ views/modules/visualizarAsistencia.php
@@ -17,6 +17,21 @@ $nombreCurso = ControllerCursos::ctrGetCurso($idCurso)["descripcionCurso"];
       </ol>
     </nav>
   </div>
+  <!-- Botones para tomar asistencia, descargar template y subir excel asistencia -->
+  <div class="row gap-3 mb-2">
+    <div class="col-lg-12">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="d-flex gap-4">
+            <button type="button" class="btn btn-primary" id="btnTomarAsistencia" data-bs-toggle="modal" data-bs-target="#modalTomarAsistencia" idCurso="<?php echo $idCurso ?>" idGrado="<?php echo $idGrado ?>" idPersonal="<?php echo $idPersonal ?>">Tomar Asistencia</button>
+            <button type="button" class="btn btn-primary" id="btnDescargarTemplateAsistencia" idCurso="<?php echo $idCurso ?>" idGrado="<?php echo $idGrado ?>" idPersonal="<?php echo $idPersonal ?>">Descargar Template</button>
+            <button type="button" class="btn btn-primary" id="btnSubirExcelAsistencia" idCurso="<?php echo $idCurso ?>" idGrado="<?php echo $idGrado ?>" idPersonal="<?php echo $idPersonal ?>">Subir Excel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
 
   <section class="section dashboard">
     <div class="row gap-3">
@@ -28,7 +43,6 @@ $nombreCurso = ControllerCursos::ctrGetCurso($idCurso)["descripcionCurso"];
               <!--  Titulo dataTableCursosDocenteAdmin-->
               <table id="dataTableAsistenciaAlumnos" class="display dataTableAsistenciaAlumnos " style="width: 100%">
                 <thead>
-
                 </thead>
                 <tbody>
                 </tbody>
@@ -39,3 +53,45 @@ $nombreCurso = ControllerCursos::ctrGetCurso($idCurso)["descripcionCurso"];
       </div>
     </div>
 </main>
+
+<!-- Modal para tomar asistencia -->
+<div class="modal fade" id="modalTomarAsistencia" tabindex="-1" aria-labelledby="modalTomarAsistenciaLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalTomarAsistenciaLabel">Tomar Asistencia</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body
+      ">
+        <div class="row gap-3">
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="card">
+                <div class="card-body">
+                  <!--  Titulo dataTableCursosDocenteAdmin-->
+                  <table id="dataTableTomarAsistencia" class="display dataTableTomarAsistencia " style="width: 100%">
+                    <thead>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="d-flex gap-4">
+              <button type="button" class="btn btn-primary" id="btnGuardarAsistencia" idCurso="<?php echo $idCurso ?>" idGrado="<?php echo $idGrado ?>" idPersonal="<?php echo $idPersonal ?>">Guardar Asistencia</button>
+              <button type="button" class="btn btn-secondary" id="btnCancelarAsistencia" data-bs-dismiss="modal">Cancelar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
