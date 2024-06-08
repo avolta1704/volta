bc7243923d969fec723448f0ee4435f7c8d8aa56
eleoromario <eleo.romario@gmail.com>
2024-06-08 11:35:47 -0500

    feat: Agregar funcionalidad para subir archivo de asistencia en formato Excel

--- ajax/asistenciaAlumnos.ajax.php
+++ ajax/asistenciaAlumnos.ajax.php
@@ -140,6 +140,131 @@ class AsistenciaAlumnosAjax
   {
     // Decodificar los datos JSON
     $data = json_decode($data, true);
+    $asistenciaAlumnosExcel = json_decode($asistenciaAlumnosExcel, true);
+
+    // Estructurar los datos de asistencia
+    $asistenciasEstructuradas = $this->estructurarDataAsistencia($asistenciaAlumnosExcel);
+
+    // Obtener todos los alumnos del curso
+    $alumnos = ControllerAlumnos::ctrGetAlumnosCurso(
+      $data["idCurso"],
+      $data["idGrado"],
+      $data["idPersonal"]
+    );
+
+    // Obtener todas las asistencias registradas para los alumnos del curso
+    $asistenciasRegistradasAlumno = ControllerAsistenciaAlumnos::ctrMostrarAsistenciaAlumnosTomarAsistenciaExcel(
+      $data["idCurso"],
+      $data["idGrado"],
+      $data["idPersonal"]
+    );
+
+    // Convertir las asistencias a registrar a un array asociativo para facilitar la búsqueda
+    $asistenciaAlumnosAssoc = [];
+    foreach ($asistenciasEstructuradas as $asistencia) {
+      $asistenciaAlumnosAssoc[$asistencia['idAlumnoAnioEscolar']][$asistencia['fechaAsistencia']] = $asistencia['estadoAsistencia'];
+    }
+
+    // Convertir las asistencias registradas a un array asociativo para facilitar la búsqueda
+    $asistenciasRegistradasAssoc = [];
+    foreach ($asistenciasRegistradasAlumno as $asistencia) {
+      $asistenciasRegistradasAssoc[$asistencia['idAlumnoAnioEscolar']][$asistencia['fechaAsistencia']] = $asistencia['estadoAsistencia'];
+    }
+
+    // Arreglo para almacenar los datos de asistencia de los alumnos
+    $asistenciaParaActualizar = [];
+
+    // Recorrer todos los alumnos del curso
+    foreach ($alumnos as $alumno) {
+      $idAlumno = $alumno["idAlumno"];
+      $idAlumnoAnioEscolar = $alumno["idAlumnoAnioEscolar"];
+
+      // Verificar las asistencias para cada fecha
+      if (isset($asistenciaAlumnosAssoc[$idAlumnoAnioEscolar])) {
+        foreach ($asistenciaAlumnosAssoc[$idAlumnoAnioEscolar] as $fechaAsistencia => $nuevoEstadoAsistencia) {
+          $existingEstadoAsistencia = isset($asistenciasRegistradasAssoc[$idAlumnoAnioEscolar][$fechaAsistencia]) ? $asistenciasRegistradasAssoc[$idAlumnoAnioEscolar][$fechaAsistencia] : null;
+
+          // Determinar si actualizar o crear una nueva asistencia
+          if ($existingEstadoAsistencia !== null) {
+            if ($nuevoEstadoAsistencia != $existingEstadoAsistencia) {
+              // Actualizar si el estado ha cambiado
+              $asistenciaParaActualizar[] = [
+                'idAlumno' => $idAlumno,
+                'idAlumnoAnioEscolar' => $idAlumnoAnioEscolar,
+                'estadoAsistencia' => $nuevoEstadoAsistencia,
+                'fechaAsistencia' => $fechaAsistencia
+              ];
+            }
+          } else {
+            // Crear nueva asistencia si no existe
+            $asistenciaParaActualizar[] = [
+              'idAlumno' => $idAlumno,
+              'idAlumnoAnioEscolar' => $idAlumnoAnioEscolar,
+              'estadoAsistencia' => $nuevoEstadoAsistencia,
+              'fechaAsistencia' => $fechaAsistencia
+            ];
+          }
+        }
+      }
+    }
+
+    // Llamar a la función del controlador para crear o actualizar las asistencias
+    $respuesta = ControllerAsistenciaAlumnos::ctrCrearActualizarAsistenciaAlumnosExcel($data["idCurso"], $data["idGrado"], $data["idPersonal"], $asistenciaParaActualizar);
+
+    // Devolver la respuesta
+    echo json_encode($respuesta);
+  }
+
+  /**
+   * Estructurar la data para tenga el estilo de alumno por cada mes y su asistencia
+   * 
+   * @param array $data array con la informacion de la asistencia de los alumnos
+   * @return array $respuesta  array con la informacion de la asistencia de los alumnos   
+   */
+  public function estructurarDataAsistencia($alumnos)
+  {
+    $separatedByDate = [];
+
+    foreach ($alumnos as $alumno) {
+      $dataAlumno = ControllerAlumnos::ctrGetAlumnoByIdAnioEscolar($alumno['IUU']);
+
+      // si hubo un error al obtener los datos del alumno, se cancelo todo
+      if (!$dataAlumno) {
+        return false;
+      }
+
+      $idAlumno = $dataAlumno['idAlumno'];
+      $idAlumnoAnioEscolar = $dataAlumno['idAlumnoAnioEscolar'];
+
+      foreach ($alumno as $key => $value) {
+        if (DateTime::createFromFormat('d/m/Y', $key) !== false) {
+          // convertir la fecha a formato Y-m-d
+          $fechaAsistencia = DateTime::createFromFormat('d/m/Y', $key)->format('Y-m-d');
+          $estadoAsistencia = $value;
+
+          $separatedByDate[] = [
+            'idAlumno' => $idAlumno,
+            'idAlumnoAnioEscolar' => $idAlumnoAnioEscolar,
+            'estadoAsistencia' => $estadoAsistencia,
+            'fechaAsistencia' => $fechaAsistencia
+          ];
+        }
+      }
+    }
+    return $separatedByDate;
+  }
+
+  /**
+   * Cancela la subida de los datos de la asistencia del excel, eliminar los datos que se registraron
+   * 
+   * @param string $data objeto con la informacion de la asistencia de los alumnos
+   * @param array $data array con la informacion de la asistencia de los alumnos
+   * @return string $respuesta  respuesta de la cancelación de la subida de la asistencia de los alumnos o error en caso de que no se haya podido realizar la operacion
+   */
+  public function ajaxCancelarAsistenciaAlumnosExcel($data, $asistenciaAlumnosExcel)
+  {
+    // Decodificar los datos JSON
+    $data = json_decode($data, true);
     $asistenciaAlumnos = json_decode($asistenciaAlumnosExcel, true);
     $asistenciaAlumnosExcel = $this->estructurarDataAsistencia($asistenciaAlumnos);
 
@@ -209,52 +334,13 @@ class AsistenciaAlumnosAjax
           ];
         }
       }
-
-      // Llamar a la función del controlador para crear o actualizar las asistencias
-      $respuesta = ControllerAsistenciaAlumnos::ctrCrearActualizarAsistenciaAlumnosExcel($data["idCurso"], $data["idGrado"], $data["idPersonal"], $asistencia);
-
-      // Devolver la respuesta
-      echo json_encode($respuesta);
     }
-  }
 
-  /**
-   * Estructurar la data para tenga el estilo de alumno por cada mes y su asistencia
-   * 
-   * @param array $data array con la informacion de la asistencia de los alumnos
-   * @return array $respuesta  array con la informacion de la asistencia de los alumnos   
-   */
-  public function estructurarDataAsistencia($alumnos)
-  {
-    $separatedByDate = [];
+    // Llamar a la función del controlador para eliminar las asistencias
+    $respuesta = ControllerAsistenciaAlumnos::ctrCancelarAsistenciaAlumnosExcel($asistencia);
 
-    foreach ($alumnos as $alumno) {
-      $dataAlumno = ControllerAlumnos::ctrGetAlumnoByIdAnioEscolar($alumno['IUU']);
-
-      // si hubo un error al obtener los datos del alumno, se cancelo todo
-      if (!$dataAlumno) {
-        return false;
-      }
-
-      $idAlumno = $dataAlumno['idAlumno'];
-      $idAlumnoAnioEscolar = $dataAlumno['idAlumnoAnioEscolar'];
-
-      foreach ($alumno as $key => $value) {
-        if (DateTime::createFromFormat('d/m/Y', $key) !== false) {
-          // convertir la fecha a formato Y-m-d
-          $fechaAsistencia = DateTime::createFromFormat('d/m/Y', $key)->format('Y-m-d');
-          $estadoAsistencia = $value;
-
-          $separatedByDate[] = [
-            'idAlumno' => $idAlumno,
-            'idAlumnoAnioEscolar' => $idAlumnoAnioEscolar,
-            'estadoAsistencia' => $estadoAsistencia,
-            'fechaAsistencia' => $fechaAsistencia
-          ];
-        }
-      }
-    }
-    return $separatedByDate;
+    // Devolver la respuesta
+    echo json_encode($respuesta);
   }
 }
 
@@ -276,4 +362,9 @@ if (isset($_POST["guardarAsistenciaAlumnos"]) && isset($_POST["asistenciaAlumnos
 if (isset($_POST["guardarAsistenciaAlumnosExcel"]) && isset($_POST["asistenciaAlumnosExcel"])) {
   $crearActualizarAsistenciaAlumnos = new AsistenciaAlumnosAjax();
   $crearActualizarAsistenciaAlumnos->ajaxCrearActualizarAsistenciaAlumnosExcel($_POST["guardarAsistenciaAlumnosExcel"], $_POST["asistenciaAlumnosExcel"]);
+}
+
+if (isset($_POST["deleteAsistenciaAlumnosExcel"]) && isset($_POST["asistenciaAlumnosExcel"])) {
+  $cancelarAsistenciaAlumnos = new AsistenciaAlumnosAjax();
+  $cancelarAsistenciaAlumnos->ajaxCancelarAsistenciaAlumnosExcel($_POST["deleteAsistenciaAlumnosExcel"], $_POST["asistenciaAlumnosExcel"]);
 }
