cc99c4d77ff3ee6e4cc9ba789a6091d53ed03e63
eleoromario <eleo.romario@gmail.com>
2024-06-08 11:36:30 -0500

    feat: Agregar método para mostrar la asistencia de los alumnos registrados en el sistema por mes

--- model/asistenciaAlumnos.model.php
+++ model/asistenciaAlumnos.model.php
@@ -93,6 +93,94 @@ class ModelAsistenciaAlumnos
   }
 
   /**
+   * Mostrar la asistencia de los alumnos registrados en el sistema
+   * 
+   * @param int $idCurso identificador del curso
+   * @param int $idGrado identificador del grado
+   * @param int $idPersonal identificador del personal
+   * @return array $respuesta  array con la informacion de la asistencia de los alumnos
+   */
+  static public function mdlMostrarAsistenciaAlumnosRegistradosMes($tabla, $idCurso, $idGrado, $idPersonal, $mesActual)
+  {
+    $tablaAlumno = "alumno";
+    $tablaAlumnoAnioEscolar = "alumno_anio_escolar";
+    $tablaNivel = "nivel";
+    $tablaGrado = "grado";
+    $tablaAnioEscolar = "anio_escolar";
+    $tablaCursoGrado = "curso_grado";
+    $tablaCurso = "curso";
+    $tablaCursoGradoPersonal = "cursogrado_personal";
+    $tablaAdmisionAlumno = "admision_alumno";
+    $statement = Connection::conn()->prepare("SELECT
+    a.idAlumno,
+    a.nombresAlumno,
+    a.apellidosAlumno,
+    g.idGrado,
+    c.idCurso,
+    cg_personal.idPersonal,
+    asis.estadoAsistencia,
+    asis.fechaAsistencia,
+    a_ae.idAlumnoAnioEscolar
+  FROM
+    $tablaAlumno as a    
+    INNER JOIN
+    $tablaAlumnoAnioEscolar as a_ae
+    ON 
+      a.idAlumno = a_ae.idAlumno
+    INNER JOIN 
+    $tablaAdmisionAlumno as admision
+    ON 
+      a.idAlumno = admision.idAlumno
+    INNER JOIN
+    $tablaGrado as g
+    ON 
+      a_ae.idGrado = g.idGrado
+    INNER JOIN
+    $tablaNivel as n
+    ON 
+      g.idNivel = n.idNivel
+    INNER JOIN
+    $tablaCursoGrado as c_g
+    ON 
+      g.idGrado = c_g.idGrado
+    INNER JOIN 
+    $tablaCurso as c
+    ON 
+      c_g.idCurso = c.idCurso
+    INNER JOIN
+    $tablaCursoGradoPersonal as cg_personal
+    ON 
+      c_g.idCursoGrado = cg_personal.idCursoGrado
+    INNER JOIN
+    $tablaAnioEscolar as ae
+    ON 
+      a_ae.idAnioEscolar = ae.idAnioEscolar
+    LEFT JOIN
+    $tabla as asis
+    ON 
+      a_ae.idAlumnoAnioEscolar = asis.idAlumnoAnioEscolar  
+      AND
+      asis.fechaAsistencia LIKE :mesActual
+  WHERE
+    g.idGrado = :idGrado
+    AND
+    c.idCurso = :idCurso
+    AND
+    cg_personal.idPersonal = :idPersonal    
+    AND
+    admision.estadoAdmisionAlumno = 2 -- 2 es el estado de alumno matriculado y activo
+    AND
+    ae.estadoAnio = 1    
+    ORDER BY a.nombresAlumno ASC");
+    $statement->bindParam(":idGrado", $idGrado, PDO::PARAM_INT);
+    $statement->bindParam(":idCurso", $idCurso, PDO::PARAM_INT);
+    $statement->bindParam(":idPersonal", $idPersonal, PDO::PARAM_INT);
+    $statement->bindValue(":mesActual", $mesActual . "%", PDO::PARAM_STR);
+    $statement->execute();
+    return $statement->fetchAll();
+  }
+
+  /**
    * Crear la asistencia del alumno para el dia actual
    * 
    * @param array $data array con la informacion de la asistencia del alumno
@@ -226,5 +314,25 @@ class ModelAsistenciaAlumnos
     $statement->bindParam(":fechaAsistencia", $fechaAsistencia, PDO::PARAM_STR);
     $statement->execute();
     return $statement->fetchAll();
+  }
+
+  /**
+   * Eliminar la asistencia del alumno 
+   * 
+   * @param string $tabla nombre de la tabla
+   * @param int $idAlumnoAnioEscolar identificador del alumno anio escolar
+   * @param string $fechaAsistencia fecha de la asistencia
+   * @return string $respuesta  respuesta de la eliminacion de la asistencia del alumno o error en caso de que no se haya podido realizar la operacion
+   */
+  static public function mdlEliminarAsistenciaAlumno($tabla, $idAlumnoAnioEscolar, $fechaAsistencia)
+  {
+    $statement = Connection::conn()->prepare("DELETE FROM $tabla WHERE idAlumnoAnioEscolar = :idAlumnoAnioEscolar AND fechaAsistencia = :fechaAsistencia");
+    $statement->bindParam(":idAlumnoAnioEscolar", $idAlumnoAnioEscolar, PDO::PARAM_INT);
+    $statement->bindParam(":fechaAsistencia", $fechaAsistencia, PDO::PARAM_STR);
+    if ($statement->execute()) {
+      return "ok";
+    } else {
+      return "error";
+    }
   }
 }
