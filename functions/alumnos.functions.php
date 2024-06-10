53470cb65810011d78756559ab78a9cfcbf06071
unknown <72468973@ucsm.edu.pe>
2024-06-10 08:56:27 -0500

    Se elimino la funcionabilidad de activar y desactivar alumno

--- functions/alumnos.functions.php
+++ functions/alumnos.functions.php
@@ -7,17 +7,17 @@ class FunctionAlumnos
     if ($estadoAlumno == 1) {
       $estado = '<span class="badge rounded-pill bg-secondary">Anulado</span>';
     } else
-    if ($estadoAlumno == 2) {
-      $estado = '<span class="badge rounded-pill bg-success">Matriculado</span>';
-    } else
-    if ($estadoAlumno == 3) {
-      $estado = '<span class="badge rounded-pill bg-warning">Trasladado</span>';
-    } else
-    if ($estadoAlumno == 4) {
-      $estado = '<span class="badge rounded-pill bg-danger">Retirado</span>';
-    } else {
-      $estado = '<span class="badge rounded-pill bg-secondary">Otro</span>';
-    }
+      if ($estadoAlumno == 2) {
+        $estado = '<span class="badge rounded-pill bg-success">Matriculado</span>';
+      } else
+        if ($estadoAlumno == 3) {
+          $estado = '<span class="badge rounded-pill bg-warning">Trasladado</span>';
+        } else
+          if ($estadoAlumno == 4) {
+            $estado = '<span class="badge rounded-pill bg-danger">Retirado</span>';
+          } else {
+            $estado = '<span class="badge rounded-pill bg-secondary">Otro</span>';
+          }
     return $estado;
   }
   //  Botones para la vista de listar alumnos
@@ -25,53 +25,29 @@ class FunctionAlumnos
   public static function getBotonesAlumnos($codAlumno, $estadoAlumno)
   {
     $botones = '
-      <div class="btn-group">
-        <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" id="dropDownAdmiAlumno" aria-expanded="false"><i class="bi bi-pencil-square"></i></button>
-        <ul class="dropdown-menu" aria-labelledby="dropDownAdmiAlumno">
-    ';
-
-    if ($estadoAlumno == 1) {
-      $botones .= '
+    <div class="btn-group">
+      <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" id="dropDownAdmiAlumno" aria-expanded="false"><i class="bi bi-pencil-square"></i></button>
+      <ul class="dropdown-menu" aria-labelledby="dropDownAdmiAlumno">
         <li><button type="button" class="dropdown-item btnVisualizarAlumno" data-bs-toggle="modal" data-bs-target="#modalViewAlumno" codAlumno="' . $codAlumno . '">Visualizar</button></li>
         <li><button type="button" class="dropdown-item btnEditarAlumno" codAlumno="' . $codAlumno . '">Editar</button></li>
-        <li><button type="button" class="dropdown-item btnDesactivarAlumno" codAlumno="' . $codAlumno . '">Desactivar</button></li>
-      ';
-    }
-    if ($estadoAlumno == 2) {
-      $botones .= '
-        <li><button type="button" class="dropdown-item btnVisualizarAlumno" data-bs-toggle="modal" data-bs-target="#modalViewAlumno" codAlumno="' . $codAlumno . '">Visualizar</button></li>
-        <li><button type="button" class="dropdown-item btnEditarAlumno" codAlumno="' . $codAlumno . '">Editar</button></li>
-        <li><button type="button" class="dropdown-item btnActivarAlumno" codAlumno="' . $codAlumno . '">Activar</button></li>
-      ';
-    }
-    if ($estadoAlumno == 3) {
-      $botones .= '
-        <li><button type="button" class="dropdown-item btnVisualizarAlumno" data-bs-toggle="modal" data-bs-target="#modalViewAlumno" codAlumno="' . $codAlumno . '">Visualizar</button></li>
-        <li><button type="button" class="dropdown-item btnEditarAlumno" codAlumno="' . $codAlumno . '">Editar</button></li>
-
-        <li><button type="button" class="dropdown-item btnEliminarAlumno" codAlumno="' . $codAlumno . '">Eliminar</button></li>
-      ';
-    }
-
-    $botones .= '
-        </ul>
-      </div>
-    ';
+      </ul>
+    </div>
+  ';
 
     return $botones;
   }
-// estado matricula para la visualizacion del alumno 
+  // estado matricula para la visualizacion del alumno 
   public static function getEstadosmatricula($estadoMatricula)
   {
     if ($estadoMatricula == null) {
       $estado = "Sin Pagado";
     }
     if ($estadoMatricula == 1) {
-      $estado =  "Pagado";
+      $estado = "Pagado";
     }
     return $estado;
   }
-// estado siagie para la visualizacion del alumno 
+  // estado siagie para la visualizacion del alumno 
   public static function getEstadoSiagie($estadoSiagie)
   {
     if ($estadoSiagie == null) {
