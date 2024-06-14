2d188a29f4b6baf16c3f1024f153fa4f464075f7
eleoromario <eleo.romario@gmail.com>
2024-06-14 13:17:48 -0500

    feat: Actualizar función getButtons para utilizar datos en formato JSON

--- functions/competencia.functions.php
+++ functions/competencia.functions.php
@@ -1,7 +1,7 @@
 <?php
 class FunctionCompetencia
 {
-  public static function getButtons($idCompetencia, $descripcionCompetencia,$idNotaCompetencia)
+  public static function getButtons($idCompetencia,  $data, $idNotaCompetencia)
   {
     //Evalue si la competencia tiene una nota asignada si es asi deshabilita el boton de eliminar
     $disabled = (!is_null($idNotaCompetencia)) ? 'disabled' : '';
@@ -12,7 +12,7 @@ class FunctionCompetencia
         <i class="bi bi-pencil-square"></i>
         </button>
         <ul class="dropdown-menu" aria-labelledby="dropDownPostulantes">
-        <li><button type="button" class="dropdown-item btnEditarCompetencias" data-bs-toggle="modal" data-bs-target="#modalEditarCompetencia" idCompetencia="' . $idCompetencia . '" descripcionCompetencia="' . $descripcionCompetencia . '">Editar</button></li>
+        <li><button type="button" class="dropdown-item btnEditarCompetencias" data-bs-toggle="modal" data-bs-target="#modalEditarCompetencia" idCompetencia="' . $idCompetencia . '" descripcionCompetencia="' . $data["descripcionCompetencia"] . '" capacidadesCompetencia="' . $data["capacidadesCompetencia"] . '"  estandarCompetencia="' . $data["estandarCompetencia"] . '">Editar</button></li>
         <li><button type="button" class="dropdown-item btnEliminarCompetencia" idCompetencia="' . $idCompetencia . '" ' . $disabled . '>Eliminar</button></li>
 
       </ul>
