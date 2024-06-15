77f7e9238cf8bf7f8f5c925d98ec8663d603b569
eleoromario <eleo.romario@gmail.com>
2024-06-15 11:02:16 -0500

    feat: Agregar método getVerCriterios a la clase FunctionCompetencia

--- functions/competencia.functions.php
+++ functions/competencia.functions.php
@@ -14,10 +14,17 @@ class FunctionCompetencia
         <ul class="dropdown-menu" aria-labelledby="dropDownPostulantes">
         <li><button type="button" class="dropdown-item btnEditarCompetencias" data-bs-toggle="modal" data-bs-target="#modalEditarCompetencia" idCompetencia="' . $idCompetencia . '" descripcionCompetencia="' . $data["descripcionCompetencia"] . '" capacidadesCompetencia="' . $data["capacidadesCompetencia"] . '"  estandarCompetencia="' . $data["estandarCompetencia"] . '">Editar</button></li>
         <li><button type="button" class="dropdown-item btnEliminarCompetencia" idCompetencia="' . $idCompetencia . '" ' . $disabled . '>Eliminar</button></li>
-        <li><button type="button" class="dropdown-item btnAbrirModalCriterios" id="btnAbrirModalCriterios" data-bs-toggle="modal" data-bs-target="#modalCriteriosCompetencia" idCompetencia="' . $idCompetencia . '" descripcionCompetencia="' . $data["descripcionCompetencia"] . '">Criterios</button></li>
+        <li></li>
       </ul>
     </div>
   ';
+    return $botones;
+  }
+
+  public static function getVerCriterios($idCompetencia, $descripcionCompetencia)
+  {
+    $botones = '<button type="button" class="btn btn-outline-success btnAbrirModalCriterios" id="btnAbrirModalCriterios" data-bs-toggle="modal" data-bs-target="#modalCriteriosCompetencia" idCompetencia="' . $idCompetencia . '" descripcionCompetencia="' . $descripcionCompetencia . '">Criterios</button>';
+
     return $botones;
   }
 }
