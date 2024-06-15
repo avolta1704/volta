45a0b7c06863f996a875ed8054b757dad6e21f3e
eleoromario <eleo.romario@gmail.com>
2024-06-15 09:26:25 -0500

    feat: Agregar funcionalidad para mostrar acciones en criterios de competencia

--- controller/criterios.controller.php
+++ controller/criterios.controller.php
@@ -11,6 +11,10 @@ class ControllerCriterios
   {
     $tabla = "criterios_competencia";
     $respuesta = ModelCriterios::mdlGetAllCriterios($tabla, $idCompetencia);
+
+    foreach ($respuesta as $key => $value) {
+      $respuesta[$key]["acciones"] = FunctionsCriterios::botonesAcciones($value["idCriterioCompetencia"]);
+    }
     return $respuesta;
   }
 
@@ -36,6 +40,30 @@ class ControllerCriterios
   {
     $tabla = "instrumento";
     $respuesta = ModelCriterios::mdlGetInstrumentosByIdTecnica($tabla, $idTecnica);
+    return $respuesta;
+  }
+
+  /**
+   * Crea un criterio.
+   * 
+   * @param int $idCompetencia El ID de la competencia.
+   * @param array $nuevoCriterio El criterio a crear.
+   * @return string $respuesta La respuesta de la creación del criterio ok si se creo y error si hubo un error.
+   */
+  public static function ctrCrearCriterio($idCompetencia, $nuevoCriterio)
+  {
+    $tabla = "criterios_competencia";
+
+    if (session_status() == PHP_SESSION_NONE) {
+      session_start();
+    }
+
+    $nuevoCriterio["usuarioCreacion"] = $_SESSION["idUsuario"];
+    $nuevoCriterio["usuarioActualizacion"] = $_SESSION["idUsuario"];
+    $nuevoCriterio["fechaCreacion"] = date("Y-m-d H:i:s");
+    $nuevoCriterio["fechaActualizacion"] = date("Y-m-d H:i:s");
+
+    $respuesta = ModelCriterios::mdlCrearCriterio($tabla, $idCompetencia, $nuevoCriterio);
     return $respuesta;
   }
 }
