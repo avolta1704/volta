da9ec0604341829f673d8d61ab07734ebe36bead
eleoromario <eleo.romario@gmail.com>
2024-06-19 15:10:14 -0500

    feat: Agregar archivos y funciones en el controller para notas de alumnos

---
+++ controller/notas.controller.php
@@ -0,0 +1,218 @@
+<?php
+class ControllerNotas
+{
+  /**
+   * Controlador para obtener todas las notas de los alumnos
+   * 
+   * @param int $idCurso id del curso
+   * @param int $idGrado id del grado
+   * @param int $idPersonal id del personal
+   * @param int $idUnidad id de la unidad
+   * @param int $idBimestre id del bimestre
+   * @return array con todas las notas de los alumnos
+   */
+  public static function ctrTodasLasNotasUnidadDeAlumnos($idCurso, $idGrado, $idPersonal, $idUnidad, $idBimestre)
+  {
+
+    // obtenemos todos los alumnos de un grado, un curso y un docente 
+    $alumnos = ControllerAlumnos::ctrGetAlumnosCurso($idCurso, $idGrado, $idPersonal);
+
+    // verificamos que haya alumnos
+    if (count($alumnos) == 0) {
+      return [];
+    }
+
+    // obtenemos todos las competencias con sus criterios de dicha unidad
+
+    $competencias = ControllerCompetencia::ctrObtenerCompetenciaCriterios($idUnidad);
+
+    // verificamos que haya competencias
+    if (count($competencias) == 0) {
+      return [];
+    }
+
+    // verificamos que cada competencia tenga al menos un criterio
+    $todosTienenCriterios = self::ctrValidarNotasCompetencias($competencias);
+
+    if ($todosTienenCriterios === 'sin criterios') {
+      return 'sin criterios';
+    }
+
+    $agruparCompetenciasPorCriterios = self::ctrAgruparCompetenciasPorCriterios($competencias);
+
+    // alumnos para agregar las notas
+    $alumnosConNotas = [];
+
+    // recorremos todos los alumnos y buscamos si tiene notas de los criterios segun las competencias
+    foreach ($alumnos as $alumno) {
+      $idAlumnoAnioEscolar = $alumno['idAlumnoAnioEscolar'];
+
+      // inicializamos el arreglo de notas
+      $alumnosConNotas[$idAlumnoAnioEscolar] = [
+        'idAlumnoAnioEscolar' => $idAlumnoAnioEscolar,
+        'nombresAlumno' => $alumno['nombresAlumno'],
+        'apellidosAlumno' => $alumno['apellidosAlumno'],
+        'notas' => []
+      ];
+
+      // recorremos las competencias agrupadas para verificar si los criterios tienen notas
+      foreach ($agruparCompetenciasPorCriterios as $idCompetencia => $criterios) {
+        $notas = [];
+
+        // recorremos los criterios
+        foreach ($criterios as $idCriterio) {
+          $nota = ModelNotas::mdlObtenerNotaAlumnoCriterio($idAlumnoAnioEscolar, $idCriterio, $idCompetencia, $idUnidad, $idBimestre);
+
+          // si no hay nota, agregamos un null pero si hay nota agregamos la nota y el id del criterio
+          if (count($nota) == 0) {
+            $notas[$idCriterio] = null;
+          } else {
+            $notas[$idCriterio] = $nota[0];
+          }
+        }
+
+        // agregamos las notas al arreglo de notas
+        $alumnosConNotas[$idAlumnoAnioEscolar]['notas'][$idCompetencia] = $notas;
+      }
+    }
+
+    // Obtenemos todos las competencias con sus criterios de la unidad
+    $competencias = ModelCompetencia::mdlObtenerCompetenciaCriteriosPorIdUnidad($idUnidad, $idBimestre);
+
+    // Verificamos que cada competencia tenga al menos un criterio
+    $todosTienenCriterios = self::ctrValidarNotasCompetencias($competencias);
+
+    if ($todosTienenCriterios === 'sin criterios') {
+      return 'sin criterios';
+    }
+
+    // Agrupar los criterios por competencia
+    $agruparCompetenciasPorCriteriosDatos = self::ctrAgruparCompetenciasPorCriteriosConDatos($competencias);
+
+    // devolver el arreglo de alumnos con notas y el arreglo de competencias con criterios
+
+    return [
+      'alumnosConNotas' => $alumnosConNotas,
+      'competencias' => $agruparCompetenciasPorCriteriosDatos
+    ];
+  }
+
+  /**
+   * Controlador para crear o actualizar una nota
+   * 
+   * @param int $idAlumnoAnioEscolar id del alumno año escolar
+   * @param int $idCriterioCompetencia id del criterio de competencia
+   * @param int $idNotaCriterio id de la nota del criterio
+   * @param string $nota nota del criterio
+   */
+  public static function ctrCrearActualizarNota($idAlumnoAnioEscolar, $idCriterioCompetencia, $idNotaCriterio, $nota)
+  {
+    // verificamos si la nota ya existe
+    $notaExistente = ModelNotas::mdlObtenerNotaAlumnoCriterioPorId($idNotaCriterio);
+
+
+
+    // si la nota existe, actualizamos la nota
+    if (count($notaExistente) > 0) {
+      // si la nota diferente a "AD", "A", "B" o "C" eliminamos la nota
+      if ($nota !== "AD" && $nota !== "A" && $nota !== "B" && $nota !== "C") {
+        $respuesta = ModelNotas::mdlEliminarNota($idNotaCriterio);
+      } else {
+        // actualizamos la nota
+        $respuesta = ModelNotas::mdlActualizarNota($idNotaCriterio, $nota);
+      }
+    } else {
+      // si la nota no existe, creamos la nota
+      $respuesta = ModelNotas::mdlCrearNota($idAlumnoAnioEscolar, $idCriterioCompetencia, $nota);
+    }
+
+    return $respuesta;
+  }
+
+  // Verificamos que todas las competencias tengan al menos un criterio
+  public static function ctrValidarNotasCompetencias($competencias)
+  {
+    $competenciasVerificadas = [];
+
+    // Recorremos el arreglo de competencias
+    foreach ($competencias as $competencia) {
+      $idCompetencia = $competencia['idCompetencia'];
+      $idCriterioCompetencia = $competencia['idCriterioCompetencia'];
+
+      // Inicializamos el arreglo si no existe
+      if (!isset($competenciasVerificadas[$idCompetencia])) {
+        $competenciasVerificadas[$idCompetencia] = [
+          'conteo' => 0,
+          'sinCriterios' => 0
+        ];
+      }
+
+      // Contamos las repeticiones y verificamos los criterios
+      $competenciasVerificadas[$idCompetencia]['conteo'] += 1;
+      if ($idCriterioCompetencia === null) {
+        $competenciasVerificadas[$idCompetencia]['sinCriterios'] += 1;
+      }
+    }
+
+    // Verificamos si alguna competencia no tiene criterios
+    foreach ($competenciasVerificadas as $idCompetencia => $datos) {
+      if ($datos['sinCriterios'] === $datos['conteo']) {
+        return 'sin criterios';
+      }
+    }
+
+
+    return 'ok';
+  }
+
+  // Agrupamos criterios por competencia
+  public static function ctrAgruparCompetenciasPorCriterios($competencias)
+  {
+    $agruparCompetenciasPorCriterios = [];
+
+    // Recorremos el arreglo de competencias
+    foreach ($competencias as $competencia) {
+      $idCompetencia = $competencia['idCompetencia'];
+      $idCriterioCompetencia = $competencia['idCriterioCompetencia'];
+
+      // Inicializamos el arreglo si no existe
+      if (!isset($agruparCompetenciasPorCriterios[$idCompetencia])) {
+        $agruparCompetenciasPorCriterios[$idCompetencia] = [];
+      }
+
+      // Guardamos los criterios en el arreglo 
+      $agruparCompetenciasPorCriterios[$idCompetencia][] = $idCriterioCompetencia;
+    }
+
+    return $agruparCompetenciasPorCriterios;
+  }
+
+  // Agrupar criterios por competencia osea solo los competencias y dentro los criterios con todos los datos
+  public static function ctrAgruparCompetenciasPorCriteriosConDatos($competencias)
+  {
+    $agruparCompetenciasPorCriterios = [];
+
+    // Recorremos el arreglo de competencias
+    foreach ($competencias as $competencia) {
+      $idCompetencia = $competencia['idCompetencia'];
+      $idCriterioCompetencia = $competencia['idCriterioCompetencia'];
+
+      // Inicializamos el arreglo si no existe
+      if (!isset($agruparCompetenciasPorCriterios[$idCompetencia])) {
+        $agruparCompetenciasPorCriterios[$idCompetencia] = [
+          'idCompetencia' => $idCompetencia,
+          'descripcionCompetencia' => $competencia['descripcionCompetencia'],
+          'criterios' => []
+        ];
+      }
+
+      // Guardamos los criterios en el arreglo
+      $agruparCompetenciasPorCriterios[$idCompetencia]['criterios'][] = [
+        'idCriterioCompetencia' => $idCriterioCompetencia,
+        'descripcionCriterio' => $competencia['descripcionCriterio'],
+      ];
+    }
+
+    return $agruparCompetenciasPorCriterios;
+  }
+}
