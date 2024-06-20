19a321154579e77cd8cafc9f772fd3ee10b8ee0e
eleoromario <eleo.romario@gmail.com>
2024-06-20 13:34:19 -0500

    feat: Corregir método para obtener ID del personal del curso y grado

--- model/unidad.model.php
+++ model/unidad.model.php
@@ -53,15 +53,13 @@ class ModelUnidad
     }
   }
 
-  public static function mdlObtenerTodoslosDatosparaAsignarNota($tabla, $idUnidadCerrar, $idAlumnoAnioEscolar)
+  public static function mdlObtenerTodoslosDatosparaAsignarNota($idUnidadCerrar, $idAlumnoAnioEscolar)
   {
     $stmt = Connection::conn()->prepare("SELECT
       nota_competencia.idNotaCompetencia, 
       nota_competencia.notaCompetencia, 
-      nota_unidad.notaUnidad, 
       competencias.idUnidad, 
       competencias.descripcionCompetencia, 
-      nota_unidad.idNotaUnidad, 
       alumno_anio_escolar.idAlumnoAnioEscolar
     FROM
       unidad
@@ -74,11 +72,6 @@ class ModelUnidad
       ON 
         competencias.idCompetencia = nota_competencia.idCompetencia
       INNER JOIN
-      nota_unidad
-      ON 
-        nota_competencia.idNotaUnidad = nota_unidad.idNotaUnidad AND
-        unidad.idUnidad = nota_unidad.idUnidad
-      INNER JOIN
       alumno_anio_escolar
       ON 
         nota_unidad.idAlumnoAnioEscolar = alumno_anio_escolar.idAlumnoAnioEscolar
@@ -90,12 +83,28 @@ class ModelUnidad
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
-  public static function mdlInsertarNotaUnidad($tabla, $idNotaUnidad, $notaUnidad, $idAlumnoAnioEscolar)
+
+  /**
+   * Insertar la nota de la unidad
+   * 
+   * @param string $tabla tabla de la base de datos
+   * @param int $idUnidad id de la unidad
+   * @param int $idAlumnoAnioEscolar id del alumno
+   * @param int $notaUnidad nota de la unidad
+   * @return string "ok" si la inserción fue exitosa, "error" si no 
+   */
+
+  public static function mdlInsertarNotaUnidad($tabla, $idUnidad, $idAlumnoAnioEscolar, $notaUnidad)
   {
-    $stmt = Connection::conn()->prepare("UPDATE $tabla SET notaUnidad = :notaUnidad WHERE idNotaUnidad = :idNotaUnidad AND idAlumnoAnioEscolar = :idAlumnoAnioEscolar");
-    $stmt->bindParam(":notaUnidad", $notaUnidad, PDO::PARAM_STR);
-    $stmt->bindParam(":idNotaUnidad", $idNotaUnidad, PDO::PARAM_INT);
+    $stmt = Connection::conn()->prepare("INSERT INTO $tabla (notaUnidad, idAlumnoAnioEscolar, idUnidad, idCursoGradoPersonal, fechaCreacion, fechaActualizacion, usuarioCreacion, usuarioActualizacion) VALUES (:notaUnidad, :idAlumnoAnioEscolar, :idUnidad, :idCursoGradoPersonal, :fechaCreacion, :fechaActualizacion, :usuarioCreacion, :usuarioActualizacion)");
+    $stmt->bindParam(":notaUnidad", $notaUnidad["notaUnidad"], PDO::PARAM_STR);
     $stmt->bindParam(":idAlumnoAnioEscolar", $idAlumnoAnioEscolar, PDO::PARAM_INT);
+    $stmt->bindParam(":idUnidad", $idUnidad, PDO::PARAM_INT);
+    $stmt->bindParam(":idCursoGradoPersonal", $notaUnidad["idCursoGradoPersonal"], PDO::PARAM_INT);
+    $stmt->bindParam(":fechaCreacion", $notaUnidad["fechaCreacion"], PDO::PARAM_STR);
+    $stmt->bindParam(":fechaActualizacion", $notaUnidad["fechaActualizacion"], PDO::PARAM_STR);
+    $stmt->bindParam(":usuarioCreacion", $notaUnidad["usuarioCreacion"], PDO::PARAM_INT);
+    $stmt->bindParam(":usuarioActualizacion", $notaUnidad["usuarioActualizacion"], PDO::PARAM_INT);
     if ($stmt->execute()) {
       return "ok";
     } else {
@@ -127,6 +136,65 @@ class ModelUnidad
   {
     $stmt = Connection::conn()->prepare("SELECT * FROM $tabla WHERE idUnidad = :idUnidad");
     $stmt->bindParam(":idUnidad", $idUnidad, PDO::PARAM_INT);
+    if ($stmt->execute()) {
+      return $stmt->fetch(PDO::FETCH_ASSOC);
+    } else {
+      return "error";
+    }
+  }
+
+  /**
+   * Obtener las competencias de una unidad
+   * 
+   * @param int $idUnidad id de la unidad
+   * @return array $dataCompetencias datos de las competencias
+   */
+  public static function mdlObtenerCompetenciasUnidad($idUnidad)
+  {
+    $stmt = Connection::conn()->prepare("SELECT * FROM competencias WHERE idUnidad = :idUnidad");
+    $stmt->bindParam(":idUnidad", $idUnidad, PDO::PARAM_INT);
+    if ($stmt->execute()) {
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+      return "error";
+    }
+  }
+
+  /**
+   * Obtener todas la nota de un competencia de un alumno
+   * 
+   * @param string $tabla tabla de la base de datos
+   * @param int $idAlumnoAnioEscolar id del alumno
+   * @param int $idCompetencia id de la competencia
+   * @return array $notaCompetencia datos de la nota de la competencia o "error" si hubo un error
+   */
+  public static function mdlObtenerNotaCompetencia($tabla, $idAlumnoAnioEscolar, $idCompetencia)
+  {
+    $stmt = Connection::conn()->prepare("SELECT idNotaCompetencia, notaCompetencia FROM $tabla WHERE idAlumnoAnioEscolar = :idAlumnoAnioEscolar AND idCompetencia = :idCompetencia");
+    $stmt->bindParam(":idAlumnoAnioEscolar", $idAlumnoAnioEscolar, PDO::PARAM_INT);
+    $stmt->bindParam(":idCompetencia", $idCompetencia, PDO::PARAM_INT);
+    if ($stmt->execute()) {
+      return $stmt->fetch(PDO::FETCH_ASSOC);
+    } else {
+      return "error";
+    }
+  }
+
+  /**
+   * Obtener las notas de la unidad de un alumno
+   * 
+   * @param string $tabla tabla de la base de datos
+   * @param int $idUnidad id de la unidad
+   * @param int $idAlumnoAnioEscolar id del alumno
+   * @param int $idCursoGradoPersonal id del curso, grado y personal
+   * @return array $notasUnidad datos de las notas de la unidad
+   */
+  public static function mdlObtenerNotaUnidad($tabla, $idUnidad, $idAlumnoAnioEscolar, $idCursoGradoPersonal)
+  {
+    $stmt = Connection::conn()->prepare("SELECT idNotaUnidad, notaUnidad FROM $tabla WHERE idUnidad = :idUnidad AND idAlumnoAnioEscolar = :idAlumnoAnioEscolar AND idCursoGradoPersonal = :idCursoGradoPersonal");
+    $stmt->bindParam(":idUnidad", $idUnidad, PDO::PARAM_INT);
+    $stmt->bindParam(":idAlumnoAnioEscolar", $idAlumnoAnioEscolar, PDO::PARAM_INT);
+    $stmt->bindParam(":idCursoGradoPersonal", $idCursoGradoPersonal, PDO::PARAM_INT);
     if ($stmt->execute()) {
       return $stmt->fetch(PDO::FETCH_ASSOC);
     } else {
