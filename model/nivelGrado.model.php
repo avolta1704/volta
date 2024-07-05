6a59683c47b0e206782898c19ac49cc2cf3599a9
eleoromario <eleo.romario@gmail.com>
2024-07-05 15:51:20 -0500

    feat: Agregar método para obtener grados por personal

--- model/nivelGrado.model.php
+++ model/nivelGrado.model.php
@@ -36,4 +36,21 @@ class ModelNivelGrado
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
   }
+
+  /**
+   * Obtener todos los grados por personal
+   * 
+   * @param int $idPersonal El id del personal.
+   * @return array/string $respuesta Retorna un array con los datos de los grados o un string con un mensaje de error.
+   */
+  public static function mdlGetGradosByPersonal($tabla, $idPersonal)
+  {
+    $tablaCursoGrado = "curso_grado";
+    $tablaGrado = "grado";
+    $tablaNivel = "nivel";
+    $statement = Connection::conn()->prepare("SELECT g.descripcionGrado, g.idGrado, n.idNivel FROM $tabla AS cg_p INNER JOIN $tablaCursoGrado as cg ON cg.idCursoGrado = cg_p.idCursoGrado INNER JOIN $tablaGrado as g ON g.idGrado = cg.idGrado INNER JOIN $tablaNivel as n ON n.idNivel = g.idNivel WHERE cg_p.idPersonal = :idPersonal");
+    $statement->bindParam(":idPersonal", $idPersonal, PDO::PARAM_INT);
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
