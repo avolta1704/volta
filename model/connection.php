c69ffe32961777356b4e8a43c78e9f8a046b6e32
David Ruben Poblette Linares <82684499+dplnares@users.noreply.github.com>
2024-11-12 13:27:02 -0500

    Credenciales base de datos

--- model/connection.php
+++ model/connection.php
@@ -7,7 +7,8 @@ class Connection
   public static function conn()
   {
     if (self::$link === null) {
-      self::$link = new PDO("mysql:host=localhost;dbname=db_volta", "root", "");
+      //self::$link = new PDO("mysql:host=localhost;dbname=db_volta", "root", "");
+      self::$link = new PDO("mysql:host=localhost;dbname=u509053799_db_volta", "u509053799_root_volta", "M@wcbFvQ9p+5");
       self::$link->exec("set names utf8");
     }
     return self::$link;
